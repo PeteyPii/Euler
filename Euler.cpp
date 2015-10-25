7ba6f649324ac2bf9daa2e6102c7af8fc7755423
Patrick Wrobel <pwrobel@uwaterloo.ca>
2015-10-24 19:26:37 -0700

    Did problem 77 (similar to problem 76)

--- Euler.cpp
+++ Euler.cpp
@@ -37,7 +37,7 @@ int32 main(int32 argc, const char **argv)
 {
 	try
 	{
-		verifyResults(76, 76);
+		verifyResults(77, 77);
 
 		return 0;
 	}
@@ -152,6 +152,7 @@ void verifyResults(int32 begin, int32 end)
 		[] () -> bool { return assertEquality(problem75(1500000), 161667); },
 
 		[] () -> bool { return assertEquality(problem76(100), 190569291); },
+		[] () -> bool { return assertEquality(problem77(5000), 71); },
 
 		[] () -> bool { return false; }
 	};
@@ -3688,6 +3689,61 @@ int32 problem76(int32 n)
 	};
 
 	return Counter(n)(n, n - 1);
+}
+int32 problem77(int32 n)
+{
+	if(n < 1)
+	{
+		return 4;
+	}
+
+	// Same strategy as problem 76 but we can only subtract primes from the sum.
+	// Let table[x][y] be the number of ways to sum to x where terms do not exceed y.
+	vector<vector<int32>> table;
+	table.push_back(vector<int32>());
+	table.push_back(vector<int32>());
+	table[0].push_back(1);
+	table[0].push_back(1);
+	table[1].push_back(0);
+	table[1].push_back(0);
+
+	vector<bool> isPrime;
+	isPrime.push_back(false);
+	isPrime.push_back(false);
+
+	int32 sum = 2;
+	while(true)
+	{
+		for(int32 i = 0; i < sum; i++)
+		{
+			table[i].push_back(table[i].back());
+		}
+
+		isPrime.push_back(isNumberPrime(sum));
+
+		table.push_back(vector<int32>());
+		table.back().push_back(0);
+		for(int32 i = 1; i <= sum; i++)
+		{
+			if(isPrime[i])
+			{
+				table.back().push_back(table[sum - i][i] + table[sum][i - 1]);
+			}
+			else
+			{
+				table.back().push_back(table[sum][i - 1]);
+			}
+		}
+
+		if(table[sum][sum] - (isPrime[sum] ? 1 : 0) > n)
+		{
+			break;
+		}
+
+		sum++;
+	}
+
+	return sum;
 }
 
 #ifdef _MSC_VER
--- Euler.h
+++ Euler.h
@@ -103,5 +103,6 @@ int32		problem74(int32 n, int32 m);	// Number of digit factorial chains with exa
 int32		problem75(int32 n);				// Number of integer sided right triangles with a unique perimeter at most n.
 
 int32		problem76(int32 n);				// Number of different ways to write n as a sum of at least two positive integers.
+int32		problem77(int32 n);				// First number that can be written as the sum of primes in over n different ways.
 
 #endif

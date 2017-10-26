910f7f0c5dc7f2223a73102e08e703f033a18413
Patrick Wrobel <craptik@gmail.com>
2017-10-25 23:15:28 -0400

    Did problem 94

--- Euler.cpp
+++ Euler.cpp
@@ -162,6 +162,7 @@ void verifyResults() {
       []() -> bool { return assertEquality(problem91(50), 14234); },
       []() -> bool { return assertEquality(problem92(10000000), 8581146); },
       []() -> bool { return assertEquality(problem93(), 1258); },
+      []() -> bool { return assertEquality(problem94(1000000000), 518408346); },
   };
 
   ifstream fin("run.txt");
@@ -3491,7 +3492,7 @@ int32 problem82(int32 n, int32 m) {
     }
   }
   idToNode[initial.m_nId] = &initial;
-  idToNode[final.m_nId] = & final;
+  idToNode[final.m_nId] = &final;
 
   map<int32, map<int32, Node*>> bestToIdToNode;
   bestToIdToNode[initial.m_nBest][initial.m_nId] = &initial;
@@ -4316,6 +4317,61 @@ int32 problem93() {
   sort(bestSet.begin(), bestSet.end());
 
   return 1000 * bestSet[0] + 100 * bestSet[1] + 10 * bestSet[2] + bestSet[3];
+}
+int64 problem94(int64 n) {
+  if (n < 1) {
+    return 0;
+  }
+
+  int64 totalPerim = 0;
+  int64 m = 1;
+  while (true) {
+    m += 1;
+    int64 m2 = m * m;
+    if (4 * m2 > n) {
+      break;
+    }
+
+    if (m % 3 == 0) {
+      continue;
+    }
+    int64 t = sqrt((m2 - 1) / 3) + 0.5;
+    if (m2 - 3 * t * t != 1) {
+      continue;
+    }
+    if ((m + t) % 2 == 0) {
+      continue;
+    }
+    if (gcd(m, t) != 1) {
+      continue;
+    }
+
+    totalPerim += 4 * m2;
+  }
+
+  m = 1;
+  while (true) {
+    m += 1;
+    int64 m2 = m * m;
+    if (2 * m2 + 4 * m + 2 > n) {
+      break;
+    }
+
+    int64 d = sqrt(3 * m2 + 1) + 0.5;
+
+    int64 t = 2 * m - d;
+    int64 t2 = t * t;
+    if (4 * m * t - m2 - t2 != -1) {
+      continue;
+    }
+    if (gcd(m, t) != 1) {
+      continue;
+    }
+
+    totalPerim += 2 * (m2 + t2) + 4 * m * t;
+  }
+
+  return totalPerim;
 }
 
 #ifdef _MSC_VER
--- makefile
+++ makefile
@@ -1,13 +1,7 @@
 CXX = g++
 CXXFLAGS = -Wall -std=c++11 -O3
 
-UNAME = $(shell uname)
-
-ifneq (,$(findstring MINGW,$(UNAME)))
-EXEC = Euler.exe
-else
 EXEC = Euler
-endif
 
 all: $(EXEC)
 
@@ -15,10 +9,10 @@ $(EXEC): Euler.cpp Euler.h Common.cpp Common.h
 	$(CXX) $(CXXFLAGS) Euler.cpp Common.cpp -o $(EXEC)
 
 test: $(EXEC)
-	$(EXEC)
+	./$(EXEC)
 
 format:
 	clang-format -i -style=file Euler.cpp Euler.h Common.cpp Common.h
 
 clean:
-	rm -f $(EXEC) *.TMP
+	rm -f $(EXEC) $(EXEC).exe *.TMP
